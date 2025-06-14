b005fc3134554ffe32ea7679a025e8ec9a0ca38b
marwin1991 <peter.zmilczak@gmail.com>
2025-06-14 19:06:54 +0200

    adjust to new hofund

--- stats/src/main/java/dev/logchange/hofund/testapp/stats/products/ProductsClient.java
+++ stats/src/main/java/dev/logchange/hofund/testapp/stats/products/ProductsClient.java
@@ -1,15 +1,27 @@
 package dev.logchange.hofund.testapp.stats.products;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
-@FeignClient(name = "products", url = "${client.products.url:http://host.docker.internal:18081/}")
-public interface ProductsClient {
+@Component
+public class ProductsClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/products")
-    List<Product> getProducts();
+    private final RestClient restClient;
 
+    public ProductsClient(@Value("${client.products.url:http://host.docker.internal:18081/}") String productsUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(productsUrl)
+                .build();
+    }
+
+    public List<Product> getProducts() {
+        return restClient.get()
+                .uri("/products")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<Product>>() {});
+    }
 }
